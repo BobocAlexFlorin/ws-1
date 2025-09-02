aafe0c118ec4aac2ffff4924c5d54c179b67a927
BobocAlexFlorin <uig99939@contiwan.com>
2025-09-02 16:38:46 +0300

    fix

--- app/app.js
+++ app/app.js
@@ -3,16 +3,18 @@ const socket = new WebSocket('ws://localhost:3000')
 function sendMessage(e) {
     e.preventDefault()
     const input = document.querySelector('input')
-    if (input.value){
+    if (input.value) {
         socket.send(input.value)
         input.value = ""
     }
     input.focus()
 }
 
-document.querySelector('form').addEventListener('submit', sendMessage)
+document.querySelector('form')
+    .addEventListener('submit', sendMessage)
 
-socket.addEventListener('message', ({ data }) => {
+// Listen for messages 
+socket.addEventListener("message", ({ data }) => {
     const li = document.createElement('li')
     li.textContent = data
     document.querySelector('ul').appendChild(li)
